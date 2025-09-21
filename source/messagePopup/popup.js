298eea1d691fe52740bbcb3bb4f0d53c7ef55b64
Heful <36966157+Heful@users.noreply.github.com>
2025-09-21 03:18:51 +0200

    Fixed Headers not being read
    No clue how to code but needed this addon to work so I just asked CoPilot. I'm sorry lol

--- source/messagePopup/popup.js
+++ source/messagePopup/popup.js
@@ -7,14 +7,16 @@
   if (!full.headers.hasOwnProperty("x-simplelogin-type") || full.headers["x-simplelogin-type"][0] !== "Forward") {
     document.querySelector("body").classList.add("not-forwarded");
   } else {
-    let from = full.headers.hasOwnProperty("x-simplelogin-envelope-from") ? full.headers["x-simplelogin-envelope-from"][0] : "unknown";
-    let to = full.headers["x-simplelogin-envelope-to"][0];
-    let deliveredTo = full.headers["delivered-to"][0];
-    let unsubscribeHref = full.headers['list-unsubscribe'][0].replace(/^<(.*)>$/, '$1');
+    let from = full.headers.hasOwnProperty("x-simplelogin-envelope-from") && Array.isArray(full.headers["x-simplelogin-envelope-from"]) ? full.headers["x-simplelogin-envelope-from"][0] : "unknown";
+    let to = full.headers.hasOwnProperty("x-simplelogin-envelope-to") && Array.isArray(full.headers["x-simplelogin-envelope-to"]) ? full.headers["x-simplelogin-envelope-to"][0] : "unknown";
+    let deliveredTo = full.headers.hasOwnProperty("x-envelope-to") && Array.isArray(full.headers["x-envelope-to"]) ? full.headers["x-envelope-to"][0] : "unknown";
+    let unsubscribeHref = (full.headers.hasOwnProperty('list-unsubscribe') && Array.isArray(full.headers['list-unsubscribe']) && full.headers['list-unsubscribe'][0])
+      ? full.headers['list-unsubscribe'][0].replace(/^<(.*)>$/, '$1')
+      : "#";
 
     document.querySelector("#from").textContent = from;
     document.querySelector("#to").textContent = to;
-    document.querySelector("#deliveredTo").textContent = deliveredTo;
+    document.querySelector("#deliveredTo").textContent = deliveredTo.replace(/[<>]/g, '');
     document.querySelector("#unsubscribe").href = unsubscribeHref + "&body=Send this e-mail to disable the alias <" + to + ">";
   }
 })();
